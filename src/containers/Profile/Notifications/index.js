fd18c057e50fce2dc2155961d4c6998c4b8d3821
LACOUTURE Ludovic <email@lacoludo.com>
2018-04-15 18:57:03 +0200

    notifications view redesigned

--- src/containers/Profile/Notifications/index.js
+++ src/containers/Profile/Notifications/index.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { StyleSheet, View } from 'react-native';
+import { Container, Content, ListItem, Radio, Separator, Text } from 'native-base';
 import { connect } from 'react-redux';
-import { Container, Content, Text, Form, Item, Label } from 'native-base';
-import { TouchableOpacity, ActivityIndicator, Button, View } from 'react-native';
-import { Actions } from 'react-native-router-flux';
 import { Permissions } from 'expo';
-import { NOTIFICATIONS_GOLD_REMINDER } from './constants';
-
-import { setReminderNotif, getReminderNotif } from '../../../actions/member';
+import { setReminderNotif } from '../../../actions/member';
 import HeaderView from './../../../components/HeaderView';
 import ButtonView from './../../../components/ButtonView';
 import MessageView from './../../../components/MessageView';
-import { scheduledNotification } from './scheduledNotifications';
+import { scheduledNotification } from './scheduledNotifications';
+
+const styles = StyleSheet.create({
+  radioRight: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+});
 
 class MyNotifications extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { reminderValue: this.props.notifications };
+    this.state = { reminderValue: this.props.notifications };
   }
 
   async componentDidMount() {
-    let result = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-    this.setState({ reminderValue: this.props.notifications });
+    const result = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    this.setState({ reminderValue: this.props.notifications });
   }
 
   onPress = (value) => {
-    this.setState({ reminderValue: value });
+    this.setState({ reminderValue: value });
   }
 
   onSubmitForm = () => {
@@ -35,71 +39,61 @@ class MyNotifications extends Component {
   }
 
   render() {
-    const { reminderValue } = this.state;
-    const { isLoading, success } = this.props;
+    const { reminderValue } = this.state;
+    const { isLoading, success } = this.props;
     return (
       <Container>
-        <Content padder>
-          <HeaderView title="Gestion des notifications" />
-          {success && <MessageView message={'Demande enregistrée.'} type={'success'}/>}
-          <Form>
-            <Label>Etre rappelé tous les</Label>
-            <View style={{ width: '100%', justifyContent: 'center', flex: 1, flexDirection: 'row'}}>
-              <TouchableOpacity
+        <HeaderView title="Gestion des notifications" />
+        {success && <MessageView message={'Demande enregistrée.'} type={'success'}/>}
+        <Content>
+          <Separator bordered style={{ marginBottom: 20 }}>
+            <Text style={{ textAlign: 'center', fontSize: 20 }}>{'Etre rappelé tous les :'.toUpperCase()}</Text>
+          </Separator>
+          <ListItem>
+            <Text>Jamais</Text>
+            <View style={styles.radioRight}>
+              <Radio
                 onPress={() => this.onPress('never')}
-                style={reminderValue === 'never' ? buttonActive : buttonInactive}
-              >
-                  <Text style={reminderValue === 'never' ? textActive : textInactive}>Jamais</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => this.onPress('week')}
-                style={reminderValue === 'week' ? buttonActive : buttonInactive}
-              >
-                <Text style={reminderValue === 'week' ? textActive : textInactive}>Semaines</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => this.onPress('month')}
-                style={reminderValue === 'month' ? buttonActive : buttonInactive}
-              >
-                <Text style={reminderValue === 'month' ? textActive : textInactive}>Mois</Text>
-              </TouchableOpacity>
+                selected={reminderValue === 'never' ? true : false}
+              />
             </View>
-            <ButtonView onPress={this.onSubmitForm} label={'Mettre à jour'} isLoading={isLoading} />
-          </Form>
+          </ListItem>
+          <ListItem>
+            <Text>Semaines</Text>
+            <View style={styles.radioRight}>
+              <Radio
+                onPress={() => this.onPress('week')}
+                selected={reminderValue === 'week' ? true : false}
+              />
+            </View>
+          </ListItem>
+          <ListItem style={{ marginBottom: 20 }}>
+            <Text>Mois</Text>
+            <View style={styles.radioRight}>
+              <Radio
+                onPress={() => this.onPress('month')}
+                selected={reminderValue === 'month' ? true : false}
+              />
+            </View>
+          </ListItem>
+          <ButtonView onPress={this.onSubmitForm} label={'Mettre à jour'} isLoading={isLoading} />
         </Content>
       </Container>
     )
   }
 }
 
-const buttonInactive = {
-  flex: 1,
-  backgroundColor: '#FFF',
-  borderColor: '#D4AF37',
-  borderWidth: 1,
-}
-
-const buttonActive = {
-  flex: 1,
-  backgroundColor: '#D4AF37',
-  borderColor: '#D4AF37',
-  borderWidth: 1,
-}
-
-const textInactive = { width: '100%', textAlign: 'center', color: '#D4AF37' };
-const textActive = { width: '100%', textAlign: 'center', color: '#FFF' };
-
 const mapStateToProps = state => ({
   isLoading: state.notifications.isLoading,
   notifications: state.member.reminderNotif,
   success: state.notifications.success,
 });
-  
+
 const mapDispatchToProps = (dispatch) => {
   return {
-    setReminderNotif: (reminder) => setReminderNotif(reminder, dispatch),
+    setReminderNotif: reminder => setReminderNotif(reminder, dispatch),
     dispatch,
   };
 };
-  
+
 export default connect(mapStateToProps, mapDispatchToProps)(MyNotifications);
